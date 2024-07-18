45758403f0c74aea28ab5f567e87e1a5ed81a36b
Omkar Khoche <141771647+omkarkhoche3448@users.noreply.github.com>
2024-07-18 23:12:36 +0530

    Update index.js

--- Day 3/index.js
+++ Day 3/index.js
@@ -115,7 +115,6 @@ checkEvenOdd(12);
 checkEvenOdd(13);
 checkEvenOdd(14);
 checkEvenOdd(15);
-checkEvenOdd(16);
 
 // Activity 5: Combining Conditions
 // - Task 7: Write a program to check if a year is a leap year using multiple conditions (divisible by 4, but not 100 unless also divisible by 400) and log the result to the console.
